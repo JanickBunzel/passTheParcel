a2713ea10c243da51c43e2ebad8bc51e29504a3f
Janick Bunzel <janick@bunzel-online.de>
2026-01-15 10:41:31 +0100

    fix button accent style

--- frontend/src/components/shadcn/button.tsx
+++ frontend/src/components/shadcn/button.tsx
@@ -12,11 +12,11 @@ const buttonVariants = cva(
                 default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 
                 outline:
-                    'border bg-white shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+                    'border bg-white shadow-xs hover:bg-gray-100 hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
 
                 secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 
-                ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+                ghost: 'hover:bg-gray-100 hover:text-accent-foreground dark:hover:bg-gray-100/50',
 
                 link: 'text-primary underline-offset-4 hover:underline',
 
